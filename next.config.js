db66a13a5eae45d1ca8b62826d805496c467688c
Hemerson Vianna <hemerson.lourenco@gmail.com>
2025-09-05 17:20:34 -0300

    chore(pages): deploy automático para branch deploy

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+const repo = 'joiascortantes';
+
 const nextConfig = {
   output: 'export',
-  images: { unoptimized: true }
+  images: { unoptimized: true },
+  trailingSlash: true, // ajuda em export estático
+  basePath: isProd ? `/${repo}` : '',
+  assetPrefix: isProd ? `/${repo}/` : undefined,
 };
+
 module.exports = nextConfig;
